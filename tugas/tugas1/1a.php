66cf9d1c21c4828c00671dcd1abdc0373d1b69ec
indritanialestari <indritania3@gmail.com>
2023-02-18 17:08:37 +0700

    mengumpulkan tugas1

---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,16 @@
+<?php 
+$variabel1 ="Topi";
+$variabel2 ="Bundar";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><i><?php echo "\"$variabel1 saya $variabel2, $variabel2 $variabel1 saya.\"" ?></i></h1>
+</body>
+</html>
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,19 @@
+<?php 
+$var1 = "9";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Aku adalah angka <b><?= $var1; ?></b></p>
+    <p>Jika aku dikali 5, maka aku sekarang menjadi <b><?= $var1 * 5; ?></b></p>
+    <p>Jika aku dibagi 2, maka aku sekarang menjadi <b><?= $var1 / 2; ?></b></p>
+    <p>Jika aku ditambah 75, maka aku sekarang menjadi <b><?= $var1 + 75; ?></b></p>
+    <p>Jika aku dikurang 20, maka aku sekarang menjadi <b><?= $var1 - 20; ?></b></p>
+</body>
+</html>
